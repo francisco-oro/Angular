62d309ac0314a2ce9062dca3cbc281ba9dafce4c
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-04-29 16:06:44 -0600

    Exposure to angular

--- 01-typescript-intro/src/topics/11-optional-chaining.ts
+++ 01-typescript-intro/src/topics/11-optional-chaining.ts
@@ -4,10 +4,20 @@ export interface Passenger {
 }
 
 const passenger1: Passenger = {
-    name: 'Francisco',
+    name: 'Fernando',
 }
 
 const passenger2: Passenger = {
     name: 'Francisco',
     children: ['Natalia', 'Elizabeth']
 }
+
+const printChildren = (passenger: Passenger): number => {
+
+    const howManyChildren = passenger.children?.length || 0;
+    console.log(passenger.name, howManyChildren);
+
+    return howManyChildren;
+}
+
+printChildren(passenger1)
