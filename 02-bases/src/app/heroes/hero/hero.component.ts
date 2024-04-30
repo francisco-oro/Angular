f2e82759290641d28ca74869a560c9f4a3585c5d
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-04-30 14:43:41 -0600

    Added support for name and age changes

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public _name: string = "ironman"
+  public age: number = 45;
 
+  get capitalizedName(): string {
+    return this._name.toUpperCase();
+
+  }
+  get getHeroDescription(): string {
+    return `${this._name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this._name = "Spiderman";
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
 }
