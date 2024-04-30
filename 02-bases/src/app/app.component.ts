b2ff8058ebda7d1d42fb30e18e7a862c12928b7b
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-04-30 12:25:22 -0600

    Added counter Component

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'My first Angular app';
+  public title: string = 'Hello World';
+  public counter: number = 10;
+
+  increaseBy( value: number ): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void{
+    this.counter = 10;
+  }
 }
