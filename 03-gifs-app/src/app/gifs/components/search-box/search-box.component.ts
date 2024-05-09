f6512d283949a89516c694ab54d403041af6b1eb
lenovo <faoe2003@gmail.com>
2024-05-08 23:40:41 -0600

    Added Gifs service

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,15 +1,25 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, ViewChild} from "@angular/core";
 
 
 @Component({
   selector: "gifs-search-box",
   template: `
-  <h1>Search: </h1>
+  <h5>Search: </h5>
   <input type="text"
   class="form-control"
-  placeholder="Search gifs..">
+  placeholder="Search gifs.."
+
+  (keyup.enter)="searchTag( )"
+         #txtTagInput
+  >
   `
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput') public txtTagInput!: ElementRef<HTMLInputElement>;
   constructor() {}
+
+  searchTag(){
+    const newTag = this.txtTagInput.nativeElement.value;
+    console.log({newTag})
+  }
 }
