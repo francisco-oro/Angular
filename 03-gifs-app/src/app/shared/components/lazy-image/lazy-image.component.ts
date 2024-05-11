f1604133678de56325e712e537976f66172bad01
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-05-11 02:12:57 -0600

    Added Lazy Image --2

---
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+})
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+
+  ngOnInit(): void {
+  }
+}
