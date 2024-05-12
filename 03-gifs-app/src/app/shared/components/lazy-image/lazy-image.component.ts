e266d7884ff85482e4acc5b5f03a0b45814937ed
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-05-12 16:43:33 -0600

    Added lazy animations do the image loader

--- 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -8,6 +8,17 @@ export class LazyImageComponent implements OnInit {
   @Input()
   public url!: string;
 
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
   ngOnInit(): void {
+    if(!this.url) throw new Error('URL property is required');
+  }
+
+  onLoad(){
+    setTimeout(() => {
+      this.hasLoaded = true;
+    }, 1000);
   }
 }
