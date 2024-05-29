6111c6788eb924e93e30b1b55fd3adcec450a4e0
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-05-28 21:08:16 -0600

    Added decimal, currency and percent pipes

--- 05-pipesApp/src/app/app.module.ts
+++ 05-pipesApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,15 @@ import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
 import {RippleModule} from "primeng/ripple";
 import {SharedModule} from "./shared/shared.module";
+
+// app's locale setup
+import localeEsHN from '@angular/common/locales/es-HN';
+import localeFrCA from '@angular/common/locales/fr-CA';
+
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localeEsHN);
+registerLocaleData(localeFrCA);
 
 @NgModule({
   declarations: [
@@ -18,7 +27,9 @@ import {SharedModule} from "./shared/shared.module";
 
     SharedModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID, useValue: 'es-HN',
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
