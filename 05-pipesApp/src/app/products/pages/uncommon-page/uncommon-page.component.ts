e2a6c0301562201c3fde5542b1e51b7ebc6e428b
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-05-28 23:00:25 -0600

    Added asyncpipe

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {interval, Observable, tap} from "rxjs";
+import {resolve} from "@angular/compiler-cli";
 
 @Component({
   selector: 'app-uncommon-page',
@@ -19,5 +21,32 @@ export class UncommonPageComponent {
   }
 
 //   i18nPlural
-  public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Hernando', 'Eduardo', 'Melissa', 'Natalia'];
+  public customers: string[] = ['Maria', 'Pedro', 'Fernando', 'Hernando', 'Eduardo', 'Melissa', 'Natalia'];
+  public customersMap = {
+    '=0': 'there are no customers waiting',
+    '=1': 'there is a customer waiting',
+    '=2': 'two customers waiting',
+    'other': 'there are # customers waiting',
+  }
+  deleteCustomer(): void {
+    this.customers.shift();
+  }
+
+  // KeyValue Pipe
+  public person = {
+    name: 'Joseph',
+    age: 23,
+    address: 'Mexico City, Mexico'
+  }
+
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap( value => console.log('tap: ', value))
+  );
+
+  public promiseValue = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('We have another promise.');
+      console.log('We have data in the promise');
+    }, 3500);
+  })
 }
