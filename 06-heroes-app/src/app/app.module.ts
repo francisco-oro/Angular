43a60b96b231dfc96ac75e1a2989c362502d03f0
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-06-08 16:34:37 -0600

    Showed heroes list

--- 06-heroes-app/src/app/app.module.ts
+++ 06-heroes-app/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from "./shared/shared.module";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import {SharedModule} from "./shared/shared.module";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     SharedModule
   ],
