fa024dbb3f79d2289a6725e163491da938184924
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-06-14 21:10:06 -0600

    Added first stages for reactive forms

--- 07-formsApp/src/app/reactive/reactive.module.ts
+++ 07-formsApp/src/app/reactive/reactive.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
@@ -10,14 +9,13 @@ import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 
 @NgModule({
   declarations: [
-    RegisterPageComponent,
     DynamicPageComponent,
     SwitchesPageComponent,
     BasicPageComponent,
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
   ]
 })
 export class ReactiveModule { }
