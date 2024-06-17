a0c405bb6dd87853d565995fe1153ee4206307d9
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-06-17 15:44:10 -0600

    Added dynamic forms component

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -28,11 +28,32 @@ export class BasicPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.myForm.reset(rtx5090)
+    // this.myForm.reset(rtx5090)
+  }
+
+  isNotValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return "This field is required";
+
+          case 'minlength':
+            return `Minimum ${errors['minlength'].requiredLength} characters is required for this field`;
+      }
+    }
+
+    return null;
   }
 
   onSave(): void {
     if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
       return;
     }
     console.log(this.myForm.value);
--- 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: ``
 })
 export class DynamicPageComponent {
+  public myForm : FormGroup = this.formBuilder.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    favoriteGames: this.formBuilder.array([
+      ['Metal Gear', [ Validators.required, Validators.min(0)]],
+      ['Death Stranding', [ Validators.required, Validators.min(0)]],
+    ])
+  })
+  constructor(private formBuilder: FormBuilder) {
+  }
 
+  onSubmit(): void {
+    if (!this.myForm.valid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+  }
 }
