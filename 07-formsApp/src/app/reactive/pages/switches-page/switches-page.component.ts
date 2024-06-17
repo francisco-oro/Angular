d46af922c38c76219a7cb7d9c7c2f77ac81cccbd
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-06-17 17:11:06 -0600

    Added switched page component

--- 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+  public myForm: FormGroup = this.formBuilder.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
 
+  public person = {
+    gender: 'F',
+    wantNotifications: false
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+  isNotValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'termsAndConditions':
+          return "You have to accept the terms and conditions of use";
+      }
+    }
+
+    return null;
+  }
+//   onSubmit
+  onSave(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
+    console.log(this.myForm.value);
+    console.log(this.person);
+  }
 }
